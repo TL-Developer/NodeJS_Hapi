0ad91a2fa41286cfd70801c849cad41bfabed309
Tiago Lima <tiago.jlima@yahoo.com.br>
2017-05-10 16:17:18 -0300

    hello world

--- index.js
+++ index.js
@@ -3,18 +3,18 @@
 const Hapi = require('hapi');
 const server = new Hapi.Server();
 
-server.connection({ port: 8080, host: '0.0.0' });
+server.connection({ port: 8080, host: '0.0.0.0' });
 
 server.route({
   method: 'GET',
   path: '/',
-  handler: function(req, res) => res({
+  handler: (req, res) => res({
     hello: 'world'
   })
 });
 
 server.start(err => {
-  if(err) throw err
+  if(err) throw err;
 
   console.log('Servidor iniciado em ', server.info.uri)
 });
